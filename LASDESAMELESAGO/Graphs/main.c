8d126232017ad56330007b18e6aec6418384fbc8
Daniel Popov <114117150+FelCon25@users.noreply.github.com>
2023-06-10 03:06:42 +0200

    Add files via upload

--- LASDESAMELESAGO/Graphs/main.c
+++ LASDESAMELESAGO/Graphs/main.c
@@ -62,16 +62,15 @@ void addEdge(Graph *g, int src, int dest, int weight) {
     }
 }
 
-Graph *addNode(Graph *g) {
+void *addNode(Graph *g) {
     Edge **tmp = NULL;
+    
     tmp = realloc(g->adj, (g->nv + 1) * sizeof(Edge *));
     if (tmp == NULL) handleError(0);
 
     g->adj = tmp;
     g->adj[g->nv] = NULL;
     g->nv += 1;
-
-    return g;
 }
 
 
@@ -89,6 +88,74 @@ void printGraph(Graph *g) {
     }
 }
 
+void removeEdge(Graph *graph, int src, int dest) {
+    Edge *curr = graph->adj[src];
+    Edge *prev = NULL;
+
+    while (curr != NULL) {
+        if (curr->key == dest) {
+            if (prev == NULL) {
+                graph->adj[src] = curr->next;
+            } else {
+                prev->next = curr->next;
+            }
+            free(curr);
+            return;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+}
+
+void dfsRec(Graph *g, int i, int *aux) {
+    Edge *e;
+    aux[i] = 1;
+    for (e = g->adj[i]; e; e = e->next)
+        if (!aux[e->key]) {
+            printf("%d,", e->key);
+            dfsRec(g, e->key, aux);
+        }
+}
+
+void dfs(Graph *g) {
+    int i;
+    int *aux = calloc(g->nv, sizeof(int));
+
+    if (!aux) handleError(0);
+    else {
+        for (i = 0; i < g->nv; i++)
+            if (!aux[i]) {
+                printf("\n%d", i);
+                dfsRec(g, i, aux);
+            }
+        free(aux);
+    }
+}
+
+void removeNode(Graph *graph, int node) {
+	int i;
+    for (i = 0; i < graph->nv; i++) {
+        removeEdge(graph, i, node);
+    }
+
+    Edge *curr = graph->adj[node];
+    while (curr != NULL) {
+        Edge *next = curr->next;
+        free(curr);
+        curr = next;
+    }
+
+    for (i = node + 1; i < graph->nv; i++) {
+        graph->adj[i - 1] = graph->adj[i];
+    }
+
+    Edge **edge = realloc(graph->adj, (graph->nv - 1) * sizeof(Edge *));
+    if (edge == NULL) handleError(0);
+    graph->adj = edge;
+    graph->nv -= 1;
+}
+
+
 void insertRandomVertex(Graph *G1, int g) {
     srand(time(NULL));
     int j = 0;
@@ -100,11 +167,20 @@ void insertRandomVertex(Graph *G1, int g) {
     }
 }
 
+
+
 int main() {
 
     Graph *G1 = createGraph(5);
     
     insertRandomVertex(G1, 10);
+    printGraph(G1);
+    
+    addNode(G1);
+    puts("\nAggiungo un nodo");
+    printGraph(G1);
+    puts("\nRimuovo un nodo");
+    removeNode(G1, 4);
     printGraph(G1);
 
     return 0;
